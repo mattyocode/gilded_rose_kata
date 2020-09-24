f41613bd4a1acca8ca9c573a9cab8c4526455d26
mattyocode <matthewoliver@live.co.uk>
2020-09-24 14:12:09 +0100

    Major refactor to use polymorphism

--- src/gilded_rose.py
+++ src/gilded_rose.py
@@ -4,54 +4,36 @@ class GildedRose:
 
     def __init__(self, items):
         self.items = items
+        self.assigner = ItemAssigner()
+
+    def item_to_class(self, items):
+        items_as_class = []
+        for x in items:
+            x = self.assigner.assign_class(x.name, x.sell_in, x.quality)
+            items_as_class.append(x)
+        return items_as_class
 
     def update_quality(self):
+        self.items = self.item_to_class(self.items)
         for item in self.items:
-            if item.name == "Aged Brie":
-                self.update_brie(item)
-            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
-                self.update_backstage(item)
-            elif item.name == "Sulfuras, Hand of Ragnaros":
-                pass
-            elif item.name == "Conjured":
-                self.update_conjured(item)
-            else:
-                self.standard_update(item)
-
-    def update_brie(self, item):
-        if item.quality < 50 and item.sell_in > 0:
-            item.quality += 1
-        elif item.quality < 50 and item.sell_in <= 0:
-            item.quality += 2
-        self.reduce_sell_in(item)
-
-    def update_backstage(self, item):
-        if item.quality < 50 and item.sell_in >= 11:
-            item.quality += 1
-        elif item.quality < 50 and 5 < item.sell_in < 11:
-            item.quality += 2
-        elif item.quality < 50 and item.sell_in < 6:
-            item.quality += 3
-        self.reduce_sell_in(item)
-        if item.sell_in < 0:
-            item.quality = 0
-
-    def standard_update(self, item):
-        if 0 < item.quality < 50:
-            item.quality -= 1
-        self.reduce_sell_in(item)
-
-    def update_conjured(self, item):
-        if 0 < item.quality < 50:
-            item.quality -= 2
-        self.reduce_sell_in(item)       
-
-    def reduce_sell_in(self,item):
-        item.sell_in -= 1
+            item.update_quality()
 
     def clear(self):
         self.items = None
 
+class ItemAssigner:
+
+    def assign_class(self, name, sell_in, quality):
+        if name == "Aged Brie":
+            return Brie(name, sell_in, quality)
+        if name == "Backstage passes to a TAFKAL80ETC concert":
+            return Backstage(name, sell_in, quality)
+        if name == "Sulfuras, Hand of Ragnaros":
+            return Sulfuras(name, sell_in, quality)
+        if name == "Conjured":
+            return Conjured(name, sell_in, quality)
+        else:
+            return Standard(name, sell_in, quality)
 
 class Item:
     def __init__(self, name, sell_in, quality):
@@ -61,3 +43,44 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+class Standard(Item):
+
+    def update_quality(self):
+        if 0 < self.quality < 50:
+            self.quality -= 1
+        self.sell_in -= 1
+
+class Conjured(Item):
+
+    def update_quality(self):
+        if 0 < self.quality < 50:
+            self.quality -= 2
+        self.sell_in -= 1 
+
+class Brie(Item):
+    
+    def update_quality(self):
+        if self.quality < 50 and self.sell_in > 0:
+            self.quality += 1
+        elif self.quality < 50 and self.sell_in <= 0:
+            self.quality += 2
+        self.sell_in -= 1
+
+class Backstage(Item):
+    
+    def update_quality(self):
+        if self.quality < 50 and self.sell_in >= 11:
+            self.quality += 1
+        elif self.quality < 50 and 5 < self.sell_in < 11:
+            self.quality += 2
+        elif self.quality < 50 and self.sell_in < 6:
+            self.quality += 3
+        self.sell_in -= 1
+        if self.sell_in < 0:
+            self.quality = 0
+
+class Sulfuras(Item):
+    
+    def update_quality(self):
+        self.quality = 80
